d28af0fdd4c374345e050fab7e489d6688c0d97b
Gavin <tianyu_0723@hotmail.com>
2019-06-06 16:48:58 +0800

    tank

---
+++ Assets/Scripts/Option.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Option : MonoBehaviour {
+    private int choice = 1;
+    public Transform posOne;
+    public Transform posTwo;
+
+    // Use this for initialization
+    void Start() { }
+
+    // Update is called once per frame
+    void Update() {
+        if (Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.UpArrow)) {
+            choice = 1;
+            transform.position = posOne.position;
+        }
+        else if (Input.GetKeyDown(KeyCode.S) || Input.GetKeyDown(KeyCode.DownArrow)) {
+            choice = 2;
+            transform.position = posTwo.position;
+        }
+
+        if (choice == 1 && Input.GetKeyDown(KeyCode.Space) || Input.GetKeyDown(KeyCode.Return)) {
+            SceneManager.LoadScene(1);
+        }
+    }
+}
